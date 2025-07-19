6d8a09d990bcf1656271b77ad49927d0e75936ed
xenomorpha <kseniaolieinikova@gmail.com>
2025-07-19 20:18:07 +0200

    Update database.py

--- database.py
+++ database.py
@@ -87,7 +87,7 @@ async def get_tasks_for_user_today(user_id: int):
         return await cursor.fetchall()
 
 # ✅ Выполненные задачи
-tasks = await database.get_completed_tasks(message.from_user.id)
+async def get_completed_tasks(user_id: int):
     async with aiosqlite.connect(DB_NAME) as db:
         cursor = await db.execute("""
             SELECT tasks.title, task_logs.timestamp
