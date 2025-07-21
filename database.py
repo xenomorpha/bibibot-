4d7cf442a2ff7b73a3eb9dd295e6f69b5eab60a9
xenomorpha <kseniaolieinikova@gmail.com>
2025-07-21 16:10:25 +0200

    Update database.py

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ async def connect():
     global _pool
     if _pool is None:
         ssl_context = ssl.create_default_context()
-        _pool = await asyncpg.create_pool(DATABASE_URL, ssl=ssl_context)
+        _pool = await asyncpg.create_pool(DATABASE_URL)
     return _pool
 
 
